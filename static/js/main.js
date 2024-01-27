931dbaafb10809caec3c8072051189b9f28c7122
w0219156 <w0219156@nscc.ca>
2024-01-27 17:16:04 -0400

    first styling add

---
+++ static/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    // $('#newsForm').on('submit', function(e){
+    //     e.preventDefault();
+    //     // AJAX call to fetch news and update the UI
+    // });
+
+    // Other interactive elements can be added here
+});
